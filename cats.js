ef0eccd2a4f9ce1c99a270988044371790f52d28
lor104 <lauren.rosentzveig@gmail.com>
2017-05-02 17:42:46 -0400

    remove extra console.log statements

--- cats.js
+++ cats.js
@@ -9,17 +9,11 @@ $(document).ready(function() {
       dataType: 'json'
     }).done(function(data) {
       var catList = data["cats"];
-      console.log(catList);
-      console.log(catList[0]["name"]);
-      console.log(catList[1]["name"]);
-      console.log(catList[2]["name"]);
-
       for (var i = 0; i < catList.length; i++) {
         cat = catList[i]
         id = '#cat' + (i + 1)
         $('<img>').attr('src', cat.photo).appendTo(id)
       }
-
     });
 
   });
