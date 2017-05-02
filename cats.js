9d24e728a57273c8fd61501c4621a272e9166a56
lor104 <lauren.rosentzveig@gmail.com>
2017-05-02 14:22:39 -0400

    set up an ajax request

--- cats.js
+++ cats.js
@@ -1,7 +1,23 @@
 $(document).ready(function() {
 
   $('.summon-cats').on('click', function() {
-    console.log('Cliked!');
+    // console.log('Cliked!');
+
+    $.ajax({
+      url: 'http://bitkittens.herokuapp.com/cats.json',
+      method: 'GET',
+      dataType: 'json'
+    }).done(function(data) {
+      var catList = data["cats"];
+      console.log(catList);
+      console.log(catList[0]["name"]);
+      console.log(catList[1]["name"]);
+      console.log(catList[2]["name"]);
+
+      
+
+    });
+
   });
 
 });
